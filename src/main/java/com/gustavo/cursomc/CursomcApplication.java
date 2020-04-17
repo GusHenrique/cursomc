948c89a8751a41538eced7add99805d78f58a662
Gustavo Henrique <gustavohenriquehq@gmail.com>
2020-04-17 22:15:44 +0200

    Criação de Cidade e Estado

--- src/main/java/com/gustavo/cursomc/CursomcApplication.java
+++ src/main/java/com/gustavo/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gustavo.cursomc.domain.Categoria;
+import com.gustavo.cursomc.domain.Cidade;
+import com.gustavo.cursomc.domain.Estado;
 import com.gustavo.cursomc.domain.Produto;
 import com.gustavo.cursomc.repositories.CategoriaRepository;
+import com.gustavo.cursomc.repositories.CidadeRepository;
+import com.gustavo.cursomc.repositories.EstadoRepository;
 import com.gustavo.cursomc.repositories.ProdutoRepository;
 
 //ComandLineRunner implementa método necessário para instanciação e associação de Objetos
@@ -22,6 +26,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -34,12 +42,20 @@ public class CursomcApplication implements CommandLineRunner{
 		//Criação de Categorias
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
 
 		//Criação de Produtos
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		//Criação de Estados
+		Estado est1 = new Estado(null, "Minas-Gerais");
+		Estado est2 = new Estado(null, "São-Paulo");
+		
+		//Criação de Cidades e Associação com seus Estados
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
 		//Adicionando Produtos às suas respectivas Categorias (Associação)
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
@@ -50,9 +66,15 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		//Adicionando Cidades aos seus respectivos Estados (Associação)
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
 		//Subindo objetos para Banco de Dados através de suas Repositories
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
